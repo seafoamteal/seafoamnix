ab86fbb39e2103b90fdde4d9357464dc98d7dd11
Hari Mohan <dev@haripm.com>
2025-12-23 16:18:27 +0000

    Add SSH public key to user config

--- users/hari/default.nix
+++ users/hari/default.nix
@@ -5,6 +5,9 @@
     initialPassword = "I'llBeThereForYou";
     extraGroups = ["networkmanager" "wheel"];
     shell = pkgs.fish;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIATMrTptuZ72X309YDzCyebkQ6We979kcoLZ3p9AwSRE"
+    ];
   };
 
   programs.fish.enable = true;
