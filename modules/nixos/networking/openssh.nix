7a50522b1dc7a075708c2a94b53519b7f91122ec
Hari Mohan <dev@haripm.com>
2025-12-24 18:17:32 +0000

    Stop OpenSSH from automatically opening firewall ports

--- modules/nixos/networking/openssh.nix
+++ modules/nixos/networking/openssh.nix
@@ -1,6 +1,7 @@
 {...}: {
   services.openssh = {
     enable = true;
+    openFirewall = false;
     settings = {
       PasswordAuthentication = false;
       PermitRootLogin = "prohibit-password";
