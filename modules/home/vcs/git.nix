3bc9a00ca9272079fa16ac492569c2fa10e5c034
Hari Mohan <dev@haripm.com>
2025-12-24 19:50:46 +0000

    Set up SSH signing for Git

--- modules/home/vcs/git.nix
+++ modules/home/vcs/git.nix
@@ -1,11 +1,29 @@
 {...}: {
+  xdg.configFile."git/allowedSigners".text = ''
+    dev@haripm.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIATMrTptuZ72X309YDzCyebkQ6We979kcoLZ3p9AwSRE
+  '';
+
   programs.git = {
     enable = true;
+
+    ignores = [
+      ".direnv/"
+    ];
+
     settings = {
       user = {
         name = "Hari Mohan";
-        email = " dev@haripm.com";
+        email = "dev@haripm.com";
+        signingKey = "~/.ssh/id_ed25519.pub";
       };
+
+      gpg = {
+        format = "ssh";
+        ssh.allowedSignersFile = "~/.config/git/allowedSigners";
+      };
+      commit.gpgSign = true;
+      tag.gpgSign = true;
+
       init = {
         defaultBranch = "trunk";
       };
