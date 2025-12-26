95c317aae5e148d9570626286c282d42e9c5b5f5
Hari Mohan <dev@haripm.com>
2025-12-26 19:47:48 +0000

    Add static ipv6 config

--- hosts/verdigris/default.nix
+++ hosts/verdigris/default.nix
@@ -18,6 +18,20 @@
     };
   };
 
+  systemd.network = {
+    enable = true;
+    networks."30-wan" = {
+      matchConfig.Name = "enp1s0";
+      networkConfig.DHCP = "ipv4";
+      address = [
+        "2a01:4f8:1c1c:3356::1/64"
+      ];
+      routes = [
+        {Gateway = "fe80::1";}
+      ];
+    };
+  };
+
   networking.hostName = "verdigris";
   time.timeZone = "Europe/Berlin";
   system.stateVersion = "25.05";
