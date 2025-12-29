7ca13ef29d2c5efc6b013fffb690064636e606be
Hari Mohan <dev@haripm.com>
2025-12-29 02:46:34 +0000

    Switch to rootful forgejo
    Otherwise Forgejo thinks it's listening on port 2222 and the clone urls
    will be wrong each time

--- hosts/verdigris/containers/forgejo.nix
+++ hosts/verdigris/containers/forgejo.nix
@@ -3,14 +3,15 @@
 {pkgs, ...}: {
   virtualisation.oci-containers.containers = {
     forgejo = {
-      # forgejo:13-rootless
-      image = "codeberg.org/forgejo/forgejo@sha256:d47936012f3bd7beca375af625d8c65580f9b781fde63893579a258d5c18f966";
+      # forgejo:13
+      image = "codeberg.org/forgejo/forgejo@sha256:88858e7f592f82d4f650713c7bed8c0cd792d7f71475a7467c5650a31cd2eda9";
       podman = {
         user = "git";
         sdnotify = "healthy";
       };
-      user = "1000:1000";
       environment = {
+        USER_UID = "1000";
+        USER_GID = "1000";
         FORGEJO__database__DB_TYPE = "sqlite3";
         FORGEJO__repository__DISABLE_STARS = "true";
         FORGEJO__server__ROOT_URL = "https://git.hari.pm";
@@ -18,8 +19,7 @@
         FORGEJO__service__DISABLE_REGISTRATION = "true";
       };
       volumes = [
-        "forgejo-data:/var/lib/gitea"
-        "forgejo-conf:/etc/gitea"
+        "forgejo-data:/data"
       ];
       extraOptions = [
         "--health-cmd=curl -f http://127.0.0.1:3000/api/healthz || exit 1"
@@ -44,9 +44,11 @@
     "ssh/git-authorised-keys" = {
       text = ''
         #!/bin/sh
-        /run/current-system/sw/bin/podman exec --interactive \
+        /run/current-system/sw/bin/podman exec \
+        --interactive \
+        --user git \
         forgejo /usr/local/bin/forgejo \
-        --config /var/lib/gitea/custom/conf/app.ini \
+        --config /data/gitea/conf/app.ini \
         keys -e git -u "$1" -t "$2" -k "$3"
       '';
       mode = "0755";
@@ -58,6 +60,7 @@
       #!/bin/sh
       /run/current-system/sw/bin/podman exec \
         --interactive \
+        --user git \
         --env SSH_ORIGINAL_COMMAND="$SSH_ORIGINAL_COMMAND" \
         forgejo \
         sh "$@"
